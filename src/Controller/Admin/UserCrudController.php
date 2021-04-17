60fa2d9e08d733b233426ca87d6265b704f4e8d6
AndryFamantanantsoa <handriamirado@gmail.com>
2021-04-18 00:20:46 +0400

    fix label

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +14,21 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('lastname', 'Nom'),
+            TextField::new('firstname', "Prénom"),
+            TextField::new('registrationNumber', 'Matricule'),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Employé')
+            ->setEntityLabelInPlural('Employés')
+            ;
+    }
+
 }
